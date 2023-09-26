fb5a4ac349b7489306bcd6078b10ec6585ffd118
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-09-26 19:34:19 -0400

    adds updated database and tables

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS store_db;
+CREATE DATABASE store_db;
+
+USE store_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL
+  salary  DECIMAL NOT NULL
+    FOREIGN KEY (department_id)
+REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL
+  last_name VARCHAR(30) NOT NULL
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+);
