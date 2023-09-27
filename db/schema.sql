1a15e04a01b0e5e286e652284b20496734a80a3e
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-09-26 20:01:31 -0400

    adds seeds

--- db/schema.sql
+++ db/schema.sql
@@ -10,18 +10,18 @@ CREATE TABLE department (
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL
-  salary  DECIMAL NOT NULL
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
     FOREIGN KEY (department_id)
 REFERENCES department(id)
 );
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL
-  last_name VARCHAR(30) NOT NULL
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   FOREIGN KEY (role_id)
-  REFERENCES role(id)
+  REFERENCES role(id),
   FOREIGN KEY (manager_id)
   REFERENCES employee(id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (name)
+VALUES ("Backend"),
+       ("Service"),
+       ("Kitchen"),
+       ("Bar");
+
+INSERT INTO role (title, salary, department)
+VALUES ("Accountant", 60000.00, 1),
+       ("Wait Staff",45000.00, 2),
+       ("Bartender",50000.00, 4),
+       ("Cook", 40000.00, 3),
+       ("Hostess",40000.00, 2),
+       ("Manager",60000.00, 1);
+
+INSERT INTO employee (title)
+VALUES ("Robin", "Banks", 1),
+       ("Lou", "Stools", 2),
+       ("Terry", "Bull", 6),
+       ("Sue", "Permann", 3),
+       ("Halee", "Louya", 4),
+       ("Anita", "Job", 5);
